c2d5a10a86345b8a4c77799eed568c0170681fed
Juan M. Tellez <juantellez64@gmail.com>
2021-01-31 20:01:28 -0800

    data-wrangling assignment notebook

--- notebook/api_data_wrangling_mini_project.py
+++ notebook/api_data_wrangling_mini_project.py
@@ -10,22 +10,6 @@
 # In[ ]:
 
 
-# Store the API key as a string - according to PEP8, constants are always named in all upper case
-
-
-# Qaundl has a large number of data sources, but, unfortunately, most of them require a Premium subscription. Still, there are also a good number of free datasets.
-
-# For this mini project, we will focus on equities data from the Frankfurt Stock Exhange (FSE), which is available for free. We'll try and analyze the stock prices of a company called Carl Zeiss Meditec, which manufactures tools for eye examinations, as well as medical lasers for laser eye surgery: https://www.zeiss.com/meditec/int/home.html. The company is listed under the stock ticker AFX_X.
-
-# You can find the detailed Quandl API instructions here: https://docs.quandl.com/docs/time-series
-
-# While there is a dedicated Python package for connecting to the Quandl API, we would prefer that you use the *requests* package, which can be easily downloaded using *pip* or *conda*. You can find the documentation for the package here: http://docs.python-requests.org/en/master/ 
-
-# Finally, apart from the *requests* package, you are encouraged to not use any third party Python packages, such as *pandas*, and instead focus on what's available in the Python Standard Library (the *collections* module might come in handy: https://pymotw.com/3/collections/).
-# Also, since you won't have access to DataFrames, you are encouraged to us Python's native data structures - preferably dictionaries, though some questions can also be answered using lists.
-# You can read more on these data structures here: https://docs.python.org/3/tutorial/datastructures.html
-
-# Keep in mind that the JSON responses you will be getting from the API map almost one-to-one to Python's dictionaries. Unfortunately, they can be very nested, so make sure you read up on indexing dictionaries in the documentation provided above.
 
 # In[51]:
 
@@ -43,7 +27,10 @@ import matplotlib.pyplot as plt
 
 # Now, call the Quandl API and pull out a small sample of the data (only one day) to get a glimpse
 # into the JSON structure that will be returned
-URL="https://www.quandl.com/api/v3/datasets/CFTC/0233CV_FO_L_ALL_CR.json"
+
+
+
+URL="https://www.quandl.com/api/v3/datasets/CFTC/0233CV_FO_L_ALL_CR.json?api_key={:s}&".format(key)
 parameters = {'start_date': '2020-01-01', 'end_date':'2020-12-31'}
 response = requests.get(URL, params=parameters)
 if response.status_code != 200:
